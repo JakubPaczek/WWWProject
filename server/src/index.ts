548bcc8508c4fb7ac322ad19bf92a8b4992c1476
Jakub Paczek <jakub.dawid.paczek@gmail.com>
2025-06-07 18:33:17 +0200

    variable fix

--- server/src/index.ts
+++ server/src/index.ts
@@ -10,7 +10,7 @@ import { createServer } from 'http';
 import { Server } from 'socket.io';
 
 const app = express();
-const PORT = 5000;
+const PORT = process.env.PORT || 5000;
 const httpServer = createServer(app);
 
 // Konfiguracja socket.io
