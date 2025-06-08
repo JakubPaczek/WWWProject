21357dc9c10be688ee5c54c1da49c06e5a3f3a6b
Jakub Paczek <jakub.dawid.paczek@gmail.com>
2025-06-09 00:36:20 +0200

    browse router -> hash router

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { HashRouter, Routes, Route } from 'react-router-dom';
 import './index.css'
 
 import LoginPage from './pages/LoginPage.tsx';
@@ -11,7 +11,7 @@ import ProtectedRoute from './components/ProtectedRoute';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <BrowserRouter basename="/WWWProject">
+    <HashRouter basename="/WWWProject">
       <Routes>
         <Route path="/" element={<LoginPage />} />
         <Route
@@ -31,6 +31,6 @@ ReactDOM.createRoot(document.getElementById('root')!).render(
           }
         />
       </Routes>
-    </BrowserRouter>
+    </HashRouter>
   </React.StrictMode>
 );
