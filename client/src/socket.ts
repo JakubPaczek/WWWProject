9d1a94639f68a3261de24b2362ed4ac1bac3ee1e
Jakub Paczek <jakub.dawid.paczek@gmail.com>
2025-06-08 20:54:42 +0200

    room page

--- client/src/socket.ts
+++ client/src/socket.ts
@@ -2,14 +2,10 @@ import { io } from 'socket.io-client';
 
 const BASE_URL = 'https://wwwproject.onrender.com';
 
-const token = localStorage.getItem('token');
-
 const socket = io(BASE_URL, {
     transports: ['websocket'], // wymusza WebSocket
     withCredentials: true,
-    auth: {
-        token: token
-    },
+    auth: { token: localStorage.getItem('token') },
 });
 
 export default socket;
