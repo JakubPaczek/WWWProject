65ea32e5f46d86aedb6034ba58317fe664246018
Jakub Paczek <jakub.dawid.paczek@gmail.com>
2025-06-08 19:30:54 +0200

    protected routes

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -3,18 +3,34 @@ import ReactDOM from 'react-dom/client'
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import App from './App.tsx'
 import './index.css'
+
 import LoginPage from './pages/LoginPage.tsx';
 import RoomsPage from './pages/RoomsPage.tsx';
 import ChatRoomWrapper from './pages/ChatRoomWrapper';
+import ProtectedRoute from './components/ProtectedRoute';
+
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    //<App />
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<LoginPage />} />
-        <Route path="/rooms" element={<RoomsPage />} />
-        <Route path="/chat/:room" element={ <ChatRoomWrapper /> } />
+        <Route
+          path="/rooms"
+          element={
+            <ProtectedRoute>
+              <RoomsPage />
+            </ProtectedRoute>
+          }
+        />
+        <Route
+          path="/chat/:room"
+          element={
+            <ProtectedRoute>
+              <ChatRoomWrapper />
+            </ProtectedRoute>
+          }
+        />
       </Routes>
     </BrowserRouter>
   </React.StrictMode>
