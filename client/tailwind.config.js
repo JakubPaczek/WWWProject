3c5cb75960c8c30e0bed2b2ef5538417f1e71500
Jakub Paczek <jakub.dawid.paczek@gmail.com>
2025-06-09 02:34:14 +0200

    safelist

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -4,6 +4,7 @@ export default {
     "./index.html",
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
+  safelist: ['bg-blue-800', 'text-white', 'text-2xl', 'shadow-lg', 'tracking-wide', 'font-light'],
   theme: {
     extend: {},
   },
